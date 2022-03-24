53f2e521fa1d01bbe05a0913b214af4456045a08
k.esposado <k.esposado@createit.com>
2022-03-24 18:41:22 +0800

    24/3/22-added image

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
+import imgLogin from "../images/login.jpg"
 
 const Wrapper = styled.div`
-    background-color: gray;
     height: 100vh;
     display: flex;
     align-items: center;
@@ -11,21 +11,28 @@ const Container = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
-    width: 100%;
-
+    max-height: 100%;
+`
+const ImgContainer = styled.div`
+    flex: 2;
 `
 const Label = styled.p`
 
 `
+const Image = styled.img`
+     max-width: 100%;
+    min-width: 100%;
+    
+`
 const Modal = styled.div`
-    height: 300px;
-    width: 500px;
+    height: 500px;
+    width: 900px;
     border: 1px solid black;
 `
 const FormContainer = styled.div`
-padding-top: 40px;
-padding-bottom: 10px;
-    
+    flex: 1;
+    padding-top: 40px;
+    padding-bottom: 10px;
 `
 const Title = styled.h1`
 `
@@ -63,8 +70,10 @@ const Login = () => {
     <Wrapper>
         
         <Modal>
-        <Title>Login</Title>
             <Container>
+            <ImgContainer>
+                <Image src={imgLogin}/>
+            </ImgContainer>
             <FormContainer>
                 <Email placeholder="Email"/>
                 <Password placeholder="Password"/>
